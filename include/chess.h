28bc1263941f0be33d5f8ac5dd3f7b59afdf2fa5
robot <fevedimitri@gmail.com>
2025-02-17 04:27:44 +0100

    added some stuff idk

--- include/chess.h
+++ include/chess.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   chess.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dfeve <dfeve@student.42.fr>                +#+  +:+       +#+        */
+/*   By: robot <robot@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/16 20:07:34 by dfeve             #+#    #+#             */
-/*   Updated: 2025/02/17 03:13:05 by dfeve            ###   ########.fr       */
+/*   Updated: 2025/02/17 04:11:54 by robot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -99,5 +99,5 @@ void	put_img_to_img(t_img *dst, t_img src, int x, int y);
 
 t_moves	*new_move(t_vector2 pos);
 t_moves	*moves_get_last(t_moves *start);
-void	moves_add_end(t_moves *start, t_moves *add);
+void	moves_add(t_moves *start, t_vector2	pos, int is_white);
 #endif
--- srcs/moves/moves_render.c
+++ srcs/moves/moves_render.c
@@ -1,31 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   pion.c                                             :+:      :+:    :+:   */
+/*   moves_render.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: robot <robot@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/17 02:52:34 by dfeve             #+#    #+#             */
-/*   Updated: 2025/02/17 04:13:45 by robot            ###   ########.fr       */
+/*   Created: 2025/02/17 04:19:59 by robot             #+#    #+#             */
+/*   Updated: 2025/02/17 04:24:35 by robot            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/chess.h"
 
-t_moves	*pion_calculate_moves(t_vector2 pos, t_piece piece)
-{
-	t_moves	*result;
-
-	if (piece == PION_B)
-	{
-		if (pos.y == 6)
-			moves_add(result, add_vec2(pos, vec2(0, 2)), 1);
-		moves_add(result, add_vec2(pos, vec2(0, 1)), 1);
-	}
-	else
-	{
-		if (pos.y == 1)
-			moves_add(result, add_vec2(pos, vec2(0, -2)), 0);
-		moves_add(result, add_vec2(pos, vec2(0, -1)), 0);
-	}
-}
